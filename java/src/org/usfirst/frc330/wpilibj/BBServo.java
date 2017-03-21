9da8587ec85c09c2155d7130c0588a95f086e391
sciencewhiz <git@rossesmail.com>
2017-03-20 20:35:01 -0700

    implement invert for other servo functions

--- java/src/org/usfirst/frc330/wpilibj/BBServo.java
+++ java/src/org/usfirst/frc330/wpilibj/BBServo.java
@@ -39,4 +39,26 @@ public class BBServo extends Servo {
 		getTable().addTableListener("Value", m_tableListener, true);
 	}*/
 
+	@Override
+	public void set(double value) {
+		if (m_invert)
+			value = 1.0 - value;
+		super.set(value);
+	}
+
+	@Override
+	public void setAngle(double degrees) {
+		if (m_invert)
+			degrees = 180.0 - degrees; //TODO change 180.0 to kMaxServoAngle
+		super.setAngle(degrees);
+	}
+
+
+	@Override
+	public void setRaw(int value) {
+		if (m_invert)
+			value = this.getRawBounds().max - value;
+		super.setRaw(value);
+	}
+
 }
